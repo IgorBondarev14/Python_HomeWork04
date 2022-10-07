1092d532e10e096f7afcd09999cb770eda2b1ef5
Igor <garkol@bk.ru>
2022-10-08 01:49:03 +0300

    Task05

--- Task05.py
+++ Task05.py
@@ -11,9 +11,7 @@
 
 # k2 = int(input("задайте степень многочлена: "))
 # m2 = 0
-# from sympy import *
 # x = Symbol('x')
-# import random
 # for i in range(0, k2 + 1):
 #     m2 = m2 + random.randint(-10, 11) * x ** i
 # print("y = ", str(m2))
@@ -27,36 +25,20 @@
 
 data1 = open('m1.txt', 'r')
 mnogochlen1 = str(*data1)
-print(mnogochlen1)
 data1.close()
 data2 = open('m2.txt', 'r')
 mnogochlen2 = str(*data2)
-print(mnogochlen2)
 data2.close()
 
 if mnogochlen2[0] != '-':
-    sum_mnogochlen = mnogochlen1 + ' + ' + mnogochlen2
+    sum_mnogochlen = str(mnogochlen1) + ' + ' + str(mnogochlen2)
 else:
-    sum_mnogochlen = mnogochlen1 + ' ' + mnogochlen2
-summa = sum_mnogochlen.split()
-print(summa)
+    sum_mnogochlen = str(mnogochlen1) + ' ' + str(mnogochlen2)
 
-index_pl = []
-for i in range(0, len(summa)):
-    if summa[i] == '+':
-        index_pl.append(i)
-for i in range(len(index_pl)):
-    del summa[int(index_pl[i])]
-print(summa)
+print(str(sum_mnogochlen))
 
-for i in range(len(summa)):
-    if summa[i] == '-':
-        summa[i + 1] = '-' + summa[i + 1]
-index_min = []
-for i in range(len(summa)):
-    if summa[i] == '-':
-        index_min.append(i)
+data_sum = open('sum_m.txt', 'w')
+data_sum.write(str(sum_mnogochlen))
+data_sum.close()
 
-for i in range(len(index_min)):
-    del summa[int(index_min[i])]
-print(index_min)
+
